b665c55050ed631fb51f7791b5daa02260275291
clickingmouse <clickingmouse@users.noreply.github.com>
2019-11-23 11:49:07 +0800

    basic golang websocket backend

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/clickingmouse/t1/gol
+
+go 1.13
+
+require github.com/gorilla/websocket v1.4.1
---
+++ backend/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/clickingmouse/t1/gol/pkg/websocket"
+)
+
+//func serveWs(w http.ResponseWriter, r *http.Request) {
+// ws, err := websocket.Upgrade(w, r)
+// if err != nil {
+// 	fmt.Fprintf(w, "%+V\n", err)
+// }
+// go websocket.Writer(ws)
+// websocket.Reader(ws)
+//}
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+	//http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+}
+
+func main() {
+	fmt.Println("Distributed T1 - GOL v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
